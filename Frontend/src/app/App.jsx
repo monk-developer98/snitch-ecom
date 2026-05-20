db207c746bd00eecfcd47e499ea36090d06aa2fd
Ashish Saxena <ashish.sxn847@gmail.com>
2026-05-20 16:48:19 +0530

    Frontend setup done

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+   
+        <div>
+          <h1 className="text-xl text-red-700">Get started</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
